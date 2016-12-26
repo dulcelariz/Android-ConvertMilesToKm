7628da76fab1b5c0b29f6ff7a6e91cde86180ff6
Dul <dulcelariz@gmail.com>
2016-12-26 17:52:06 -0600

    Add user interface and functionality

--- app/src/main/java/com/example/dulcelariz/convertmilestokmpro/MainActivity.java
+++ app/src/main/java/com/example/dulcelariz/convertmilestokmpro/MainActivity.java
@@ -1,13 +1,99 @@
 package com.example.dulcelariz.convertmilestokmpro;
 
+import android.icu.text.DecimalFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonConvMilesToKm = (Button) findViewById(R.id.ButtonConvertMilesToKm);
+        buttonConvMilesToKm.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                EditText textBoxMiles = (EditText) findViewById(R.id.editTextMiles);
+                EditText textBoxKm = (EditText) findViewById(R.id.editTextKm);
+                double vMiles = Double.valueOf(textBoxMiles.getText().toString());
+                double vKm = vMiles / 0.62137;
+                DecimalFormat formatVal = new DecimalFormat("##.##");
+                textBoxKm.setText(formatVal.format(vKm));
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        Button buttonConvKmToMiles = (Button) findViewById(R.id.buttonConvertKmToMiles);
+        buttonConvKmToMiles.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                EditText textBoxMiles = (EditText) findViewById(R.id.editTextMiles);
+                EditText textBoxKm = (EditText) findViewById(R.id.editTextKm);
+                double vKm = Double.valueOf(textBoxKm.getText().toString());
+                double vMiles = vKm * 0.62137;
+                DecimalFormat formatVal = new DecimalFormat("##.##");
+                textBoxMiles.setText(formatVal.format(vMiles));
+            }
+
+        });
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
